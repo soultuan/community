e51c145549a64a5ff6160faf6e76b202bc633c9d
chenkewen <763302385@qq.com>
2025-04-07 20:39:24 +0800

    统一记录日志

--- src/main/java/com/tuanzisama/community/CommunityApplication.java
+++ src/main/java/com/tuanzisama/community/CommunityApplication.java
@@ -3,6 +3,7 @@ package com.tuanzisama.community;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.tuanzisama.community.mapper"})
---
+++ src/main/java/com/tuanzisama/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,34 @@
+package com.tuanzisama.community.aspect;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.tuanzisama.community.service.*.*(..))")
+    public void pointcut() {}
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        String remoteHost = request.getRemoteHost();
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String method = joinPoint.getSignature().getDeclaringTypeName() + "." +joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s]在[%s]访问了[%s]", remoteHost, time, method));
+    }
+}
